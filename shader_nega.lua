b39d11650714de4eb831b0179d53539e0e5129e4
Koi <61906304+OxBrutal@users.noreply.github.com>
2026-05-01 18:52:03 +0700

    Update shader_nega.lua

--- shader_nega.lua
+++ shader_nega.lua
@@ -1,11 +1,3 @@
---         __           .__         ___.                        
---   ____ |  | ______   |__| ______ \_ |__   ____  ______ ______
--- _/ __ \|  |/ /  _ \  |  |/  ___/  | __ \ /  _ \/  ___//  ___/
--- \  ___/|    <  <_> ) |  |\___ \   | \_\ (  <_> )___ \ \___ \ 
---  \___  >__|_ \____/  |__/____  >  |___  /\____/____  >____  >
---      \/     \/               \/       \/           \/     \/ 
--- join the discord to dump scripts https://discord.gg/yRrH6vHBCK
-
 local Lighting = game:GetService("Lighting")
 local Players = game:GetService("Players")
 local BloomEffect = Instance.new("BloomEffect")
